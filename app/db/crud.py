0bb6c098db3b3945ad752fec81ea3b3ad6cfd72e
Anne68 <134500569+Anne68@users.noreply.github.com>
2025-08-28 19:43:08 +0100

    Delete app directory

--- app/db/crud.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.orm import Session
-
-def get_game_titles(db: Session, limit: int = 10):
-    # TODO: implémentation réelle (SELECT ... FROM games)
-    return ["Sample Game A", "Sample Game B"][:limit]
